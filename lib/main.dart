e081a2ce585cd4aa79be5cacb798c71b6c62092d
Emran Deniz <63171566+EmranDenizz@users.noreply.github.com>
2022-12-30 01:18:02 +0300

    first update.

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,43 @@ class QuizShow extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10),
+            child: QuestionPage(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class QuestionPage extends StatefulWidget {
+  const QuestionPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuestionPage> createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Yarışma Soruları',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
